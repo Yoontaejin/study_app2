820dee8365d2cb9804ccd61dae9195200f24216b
taejin <nicetaejin@rosesystems.kr>
2020-01-29 11:08:17 +0900

    Updates

---
+++ precache-manifest.3c8150d939f011e5294c7a5929c6f4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b226b7df7e9eae94ac7f0518bb177b6c",
+    "url": "/Yoontaejin/study_app2/index.html"
+  },
+  {
+    "revision": "3a1a02d9ab3ea0bbb1f3",
+    "url": "/Yoontaejin/study_app2/static/css/main.67207ceb.chunk.css"
+  },
+  {
+    "revision": "fee9de5509b1a62eb46d",
+    "url": "/Yoontaejin/study_app2/static/js/2.b816511c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Yoontaejin/study_app2/static/js/2.b816511c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a1a02d9ab3ea0bbb1f3",
+    "url": "/Yoontaejin/study_app2/static/js/main.f7255a22.chunk.js"
+  },
+  {
+    "revision": "5cdec43227985ac9e5bc",
+    "url": "/Yoontaejin/study_app2/static/js/runtime-main.ef4d2d74.js"
+  }
+]);
